001142260d5bffe23b83d28c445b9e5cc836f5ea
CodexPremiera <ashleyken.professional@gmail.com>
2025-05-30 00:06:09 +0800

    fetch and render messages

---
+++ components/ListMessages.tsx
@@ -0,0 +1,18 @@
+"use client";
+import React from "react";
+import {useMessage} from "@/lib/store/messages";
+import Message from "@/components/Message";
+
+export default function ListMessages() {
+  const messages = useMessage((state) => state.messages);
+
+  return (
+    <div className="flex-1 flex flex-col justify-end p-5 h-full overflow-y-auto scrollbar-thin">
+      <div className="space-y-7">
+        {messages.map((value, index) => {
+          return <Message key={index} message={value} />;
+        })}
+      </div>
+    </div>
+  );
+}
