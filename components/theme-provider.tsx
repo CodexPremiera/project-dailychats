6c4ed87460c52608a508cc3c92999620145caac3
CodexPremiera <ashleyken.professional@gmail.com>
2025-06-08 16:13:17 +0800

    ultimate resort

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,9 +1,14 @@
 "use client";
 
 import * as React from "react";
-import { ThemeProvider as NextThemesProvider, ThemeProviderProps } from "next-themes";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
 
-interface Props extends ThemeProviderProps {}
+interface Props extends React.PropsWithChildren {
+  attribute?: string;
+  defaultTheme?: string;
+  enableSystem?: boolean;
+  disableTransitionOnChange?: boolean;
+}
 
 export function ThemeProvider({ children, ...props }: Props) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
