fc344b6eb5032a1c3851451ee53d39242aaa1af4
CodexPremiera <ashleyken.professional@gmail.com>
2025-06-08 16:07:15 +0800

    fix theme-provider

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,14 +1,9 @@
 "use client";
 
 import * as React from "react";
-import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { ThemeProvider as NextThemesProvider, ThemeProviderProps } from "next-themes";
 
-interface Props extends React.PropsWithChildren {
-  attribute?: string;
-  defaultTheme?: string;
-  enableSystem?: boolean;
-  disableTransitionOnChange?: boolean;
-}
+interface Props extends ThemeProviderProps {}
 
 export function ThemeProvider({ children, ...props }: Props) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
